e85c04e8197a8513827bca1cb2a63053a4a7b837
tomcl <tomcl@users.noreply.github.com>
2019-02-19 12:37:00 +0000

    add documentation, refactor module structure

--- parsing/Program.fs
+++ parsing/Program.fs
@@ -10,18 +10,22 @@ module Gen =
     // custom random generators using FsCheck.Gen combinators
     // See https://github.com/haf/expecto/blob/master/README.md#property-based-tests
     // See https://fscheck.github.io/FsCheck/TestData.html for Gen.* info
-    let alphaGen = Gen.elements ([ 'a'..'z'] @ ['A'..'Z'])
+    let alphaGen = Gen.elements ([ 'a'..'z'] @ ['A'..'Z']) // choose random element of list
     let digitGen = Gen.elements(['0'..'9'])
-    let alphaNumGen = Gen.oneof [alphaGen ; digitGen] 
+    let alphaNumGen = Gen.oneof [alphaGen ; digitGen] // uniform randomly select a generator from list
     let regNameGen = Gen.map (sprintf "R%d") (Gen.oneof [Gen.choose(0,15)])
     let manyAlphaNumGen = 
-        let arrToStr a = a |> Seq.map Char.ToString |> String.concat ""
-        Gen.map arrToStr  (Gen.arrayOf alphaNumGen)
+        // NB the simpler a |> Seq.ofArray |> string here does not work. Bug?
+        let arrToStr a = a |> Array.map Char.ToString |> String.concat "" // convert array of Char to string
+        Gen.arrayOf alphaNumGen // randomly sized array of alphaNumGen (length scales with 'size' parameter)
+        |> Gen.map arrToStr // map result of generator to make new generator
     let idGen = Gen.map2 (fun (a:char) b -> 
-        a.ToString() + b) alphaGen manyAlphaNumGen
-    let opGen = Gen.elements tokenOpArray
-    let numGen = Gen.map (sprintf "%d") (Gen.choose(0,9999999))
-    let tokListGen = Gen.listOf (Gen.oneof [idGen ; idGen ; numGen; numGen; opGen])
+        a.ToString() + b) alphaGen manyAlphaNumGen // combine initial letter and variable number of alphanum
+    let opGen = Gen.elements tokenOpArray // random choice from list
+    let numGen = Gen.map (sprintf "%d") (Gen.choose(0,9999999)) // random choice from integer range
+    let tokListGen = 
+        Gen.oneof [idGen ; idGen ; numGen; numGen; opGen] // randomly choose token type
+        |> Gen.listOf // generate random length lists of tokens (similarly to arrayOf)
     let optSpaceGen = Gen.elements ["";" "]
 
     // 'partial active pattern' used in tokStringArb
@@ -31,35 +35,45 @@ module Gen =
         if Array.contains s tokenOpArray then None else Some s
 
     /// Custom generator of even integers
+    /// Every custom generator needs a separate D.U. type
     type EvenInt = EvenInt of int
     let evenIntArb = 
-        Gen.choose(1,100) 
-        |> Gen.filter (fun i -> i % 2 = 0)
+        Gen.choose(1,100) // random choice from 1..100
+        |> Gen.filter (fun i -> i % 2 = 0) // filter random numbers
+        // boilerplate to make 'Arbitrary' wrapped by D.U. for use in FsCheck
         |> Arb.fromGen
         |> Arb.convert EvenInt (fun (EvenInt l) -> l)
 
     /// Custom generator of valid strings for tokenisation
     /// Use gen {} comp expression for complex generation
     /// NB this will NOT normally be needed
+    /// Every custom generator needs a separate D.U. type
     type TokString = TokString of string list
     let tokStringArb =
-        gen {
-            let! tokList = tokListGen
-            let! sepList = Gen.listOfLength tokList.Length optSpaceGen
-            let toksWithOptSeps = List.zip tokList sepList
+        gen { // create next value of a Gen type RNG: uses F# computation expression syntax
+            let! tokList = tokListGen // let! gets one random sample from Gen on RHS
+            let! sepList = Gen.listOfLength tokList.Length optSpaceGen // one random sample of list of opt spaces
+            /// outputs string of next token with trailing space if needed
+            /// operators before or after other tokens do NOT need a separating space
             let getNextTokWithSep (lst: (string * string) list) =
+                // called with list of pairs (t,s): t is token string, s is randomly either "" or " "
                 match lst with
                 | [] | [_] -> ""
                 | (MatchNotOp t1, s1) :: (MatchNotOp _, _) :: _ -> t1 + " " // in this case a space is needed
                 | (t1, s1) :: (_, _) :: _ -> t1 + s1 // in this case an operator means no space is needed
-            let rec subLists  = function | [] -> [[]] | _ :: tl as lst  -> lst :: subLists tl
-            // return the list of tokens (with spaces added)
+                                                     // use sample from optSpaceGen to insert spaces randomly
+            /// generate successive tails of input list
+            let rec listTails  = function | [] -> [[]] | _ :: tl as lst  -> lst :: listTails tl
+            // return a list of strings representing input tokens with spaces added
+            // this outputs the next RNG sample from the Gen value defined by this computation expression
+            // NB return is necessary because this is a computation expression
             return 
-                toksWithOptSeps 
-                |> subLists
+                List.zip tokList sepList
+                |> listTails
                 |> List.map getNextTokWithSep
         }
-        |> Arb.fromGen // wrap the custon Generator type as an Arbitrary for use with FsCheck
+        // boilerplate to make Arbitrary wrapped in D.U.
+        |> Arb.fromGen // wrap the custom Gen type as an Arbitrary for use with FsCheck
         |> Arb.convert TokString (fun (TokString l) -> l) // Wrap the string list in TokString case
 
     let addToConfig config =
@@ -80,14 +94,16 @@ module Auto =
     let etestProp stdgen name = etestPropertyWithConfig stdgen config name
 
 /// show 10 samples from FsCheck custom Arbitrary
-let printArbSamples (arb:Arbitrary<'a>) =
-    let lst = (Gen.sample 7 10 arb.Generator)
-    lst |> List.map (printf "%A\n")
+let printArbSamples mess (arb:Arbitrary<'a>) =
+    printfn "%s" mess
+    let lst = (Gen.sample 3 10 arb.Generator)
+    lst |> List.map (printf "\t%A\n")
     |> ignore
     printfn ""
 
 /// show 10 samples from FsCheck custom generator
-let printGenSamples gen = Arb.fromGen gen |> printArbSamples
+let printGenSamples mess gen = 
+    Arb.fromGen gen |> printArbSamples mess
 
 let tokRes = tokenise tokenOpArray tokenEndStrings
 
@@ -105,7 +121,8 @@ let testCustomTokeniser =
             match tokRes tokStr with
             | Ok lst -> Expect.equal lst.Length tokLst.Length 
                          (sprintf "Tokenise '%A' = %A" tokStr lst)
-            | Error mess -> failwithf "Tokeniser failed with:%s" mess
+            | Error mess -> Expect.isTrue false (sprintf "Tokeniser failed with:%s" mess)
+
         testProp "Tokens are the same as input string" <| fun (Gen.TokString ct) ->
             let tokStr = String.concat "" ct
             let tokInStr (tok:Token) = 
@@ -116,11 +133,8 @@ let testCustomTokeniser =
             |> Result.map (List.iter tokInStr)
             |> function
                  | Ok _ -> ()
-                 | Error mess -> failwithf "%s" mess
+                 | Error mess -> Expect.isTrue false mess
         ]
-
-
-
 
 /// Test the Expecto Test Framework!
 [<Tests>]
@@ -128,6 +142,7 @@ let allTests = testList "Expecto tests" [
     testCase "A sample test" <| fun () ->
         let expected = 4
         Expect.equal expected (2+2) "2+2 = 4"
+
     testCase "Tokeniser sanity test" <| fun () ->
         let src = "ADD R1,R2"
         let expected = Ok [
@@ -136,9 +151,10 @@ let allTests = testList "Expecto tests" [
             {Pos=6 ; Text="," ; TokType=OpTok}
             {Pos=7 ; Text="R2" ; TokType=SymTok} ]
         Expect.equal expected (tokRes src) "Four tokens correct"
+
     testCase "Tokeniser insanity test" <| fun () ->
         let src = "MOV 0abc"
-        Expect.isError (tokRes src) "Symbols must start with letter"
+        Expect.isError (tokRes src) "Symbols starting with digit make error"
     ]
 
 [<Tests>]
@@ -164,8 +180,10 @@ let allFsChecks =
     // tagged by [<Tests>]
     Tests.runTests expectoConfig properties
 
+// printable string equiv of tokens
 let dispTok (tok:Token) = sprintf "<%A|'%s':%d>" tok.TokType tok.Text tok.Pos
 
+// printable string equivalent of a list of tokens
 let dispTLst lst = lst |> List.map dispTok |> String.concat " ; " |> sprintf "[%s]"
 
 
@@ -194,10 +212,21 @@ let parseSomething() =
 
 [<EntryPoint>]
 let main argv =
+    // run all tests tagged with [<TESTS>] - can be in any file
     runTestsInAssembly expectoConfig [||] |> ignore // run all tests defined with [<Tests>]
-    printArbSamples Gen.tokStringArb
-    tokeniseSomething()
-    parseSomething()
+    // print samples from the custom tokenise input generator
+    // NB to get the input string concatenate the list of strings with empty separator
+    //printGenSamples "alpha" Gen.alphaGen
+    //printGenSamples "regName" Gen.regNameGen
+    //printGenSamples "manyAlphaNum" Gen.manyAlphaNumGen
+    //printGenSamples "id" Gen.idGen
+    //printArbSamples "tokStringArb" Gen.tokStringArb
+    //printGenSamples "tokListGen" Gen.tokListGen
+    // run the tokeniser, printing the result
+    //tokeniseSomething()
+    // run the parser, printing the result
+    //parseSomething()
+    // keep command window open
     printfn "press any key to terminate"
     Console.ReadKey() |> ignore
     0 // return an integer exit code
